b691274abf2bbe34326620fa8cd4c9b1eb248fe4
abhay <abhaytxanax@gmail.com>
2023-02-02 15:58:41 +0530

    header.jsx

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import Image from 'next/image'
+import HeaderItem from './HeaderItem'
 
 const Header = () => {
   return (
     <header>
-      
+      <div>
+        {/* <HeaderItem title='Home' Icon /> */}
+      </div>
+      <Image src="https://links.papareact.com/ua6" 
+        width={200}
+        height={200}
+      />
     </header>
   )
 }
---
+++ components/HeaderItem.js
@@ -0,0 +1,10 @@
+
+const HeaderItem = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HeaderItem

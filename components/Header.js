4097272da5fe9054f3a2e6aaa04dc16e4bc773bb
abhay <abhaytxanax@gmail.com>
2023-02-02 16:27:56 +0530

    header-component

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,25 @@
 import Image from 'next/image'
 import HeaderItem from './HeaderItem'
 
+import {
+    BadgeCheckIcon,
+    CollectionIcon,
+    HomeIcon,
+    LightningBoltIcon,
+    SearchIcon,
+    UserIcon,
+} from "@heroicons/react/outline"
+
 const Header = () => {
   return (
-    <header>
-      <div>
-        {/* <HeaderItem title='Home' Icon /> */}
+    <header className='flex flex-col sm:flex-row justify-between m-5 items-center'>
+      <div className='flex flex-grow justify-evenly max-w-2xl'>
+        <HeaderItem title='HOME' Icon={HomeIcon} />
+        <HeaderItem title='TRENDING' Icon={LightningBoltIcon} />
+        <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon} />
+        <HeaderItem title='COLLECTIONS' Icon={CollectionIcon} />
+        <HeaderItem title='SEARCH' Icon={SearchIcon} />
+        <HeaderItem title='ACCOUNT' Icon={UserIcon} />
       </div>
       <Image src="https://links.papareact.com/ua6" 
         width={200}
--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -1,8 +1,9 @@
 
-const HeaderItem = () => {
+const HeaderItem = (props) => {
   return (
-    <div>
-      
+    <div className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white">
+      <props.Icon className="h-8 mb-1 group-hover:animate-bounce" />
+      <p className="opacity-0 group-hover:opacity-100">{props.title}</p>
     </div>
   )
 }

e033e33ed2c2c631f0922e9cbca7a9bc9fa76a70
abhay <abhaytxanax@gmail.com>
2023-02-17 15:56:01 +0530

    ui-components

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
-import Header from '@/components/Header'
-import Nav from '@/components/Nav'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,14 +12,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      {/* header */}
-      <Header />
-
-      {/* nav */}
-      <Nav />
-
-      {/* results */}
+      
     </>
   )
 }
